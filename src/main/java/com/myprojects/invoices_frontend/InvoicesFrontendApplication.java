463c880c68f11a8de4dbd2af55960b8a93325ed2
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2022-01-13 14:59:30 +0100

    Start of the project

---
+++ src/main/java/com/myprojects/invoices_frontend/InvoicesFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.invoices_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicesFrontendApplication.class, args);
+    }
+
+}
